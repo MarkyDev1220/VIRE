5d0702640f1b797677ce554a7de05aeed06e1c09
marke <markeviandjones@gmail.com>
2025-11-22 00:50:14 -0500

    WorkBench 2 Commit

---
+++ VireApp/src/main/java/com/example/vire/android/ChatAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.vire.android
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ChatAdapter(
+    private val messages: MutableList<Message>
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        private const val VIEW_ME = 0
+        private const val VIEW_OTHER = 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (messages[position].sender == "Me") VIEW_ME else VIEW_OTHER
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return if (viewType == VIEW_ME) {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_message_me, parent, false)
+            MeMessageViewHolder(view)
+        } else {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_message_other, parent, false)
+            OtherMessageViewHolder(view)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val message = messages[position]
+        when (holder) {
+            is MeMessageViewHolder -> holder.bind(message)
+            is OtherMessageViewHolder -> holder.bind(message)
+        }
+    }
+
+    override fun getItemCount() = messages.size
+
+    fun notifyNewMessage() {
+        notifyItemInserted(messages.size - 1)
+    }
+
+    class MeMessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val msgText: TextView = itemView.findViewById(R.id.messageTextMe)
+        fun bind(message: Message) {
+            msgText.text = message.content
+        }
+    }
+
+    class OtherMessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val msgText: TextView = itemView.findViewById(R.id.messageTextOther)
+        fun bind(message: Message) {
+            msgText.text = message.content
+        }
+    }
+}
+
+
