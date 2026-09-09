cfe4be40b51f3334cf578a0b6c6469972f081a16
Your Name <you@example.com>
2026-09-09 03:26:35 -0400

    Recommited with changes

--- VireApp/build.gradle.kts
+++ VireApp/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 2
-        versionName = "1.1"
+        versionName = "1.6"
     }
     buildFeatures {
         viewBinding = true
